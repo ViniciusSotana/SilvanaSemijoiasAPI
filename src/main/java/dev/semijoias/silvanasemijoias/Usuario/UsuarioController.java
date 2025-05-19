29210f74a3744f5f1d39db78098a55157e684a0c
ViniciusSotana <vinysotana@gmail.com>
2025-05-19 00:34:36 -0300

    feat(usuario): criar endpoints no UsuarioController

--- src/main/java/dev/semijoias/silvanasemijoias/Usuario/UsuarioController.java
+++ src/main/java/dev/semijoias/silvanasemijoias/Usuario/UsuarioController.java
@@ -1,4 +1,62 @@
-package dev.Semijoias.SilvanaSemijoias.Usuario;
+package dev.semijoias.silvanasemijoias.Usuario;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioService.listarUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> buscarPorId(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.buscarPorId(id);
+        if (usuario != null) {
+            return ResponseEntity.ok(usuario);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> criarUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+        UsuarioDTO usuarioCriado = usuarioService.cadastrarUsuario(usuarioDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> atualizarUsuario(@PathVariable Long id, @RequestBody @Valid UsuarioDTO usuarioDTO) {
+        UsuarioDTO usuarioAtualizado = usuarioService.atualizarUsuario(id, usuarioDTO);
+        if (usuarioAtualizado != null) {
+            return ResponseEntity.ok(usuarioAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> removerUsuario(@PathVariable Long id) {
+        if (usuarioService.buscarPorId(id) != null) {
+            usuarioService.removerUsuario(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        }
+    }
+
 }
